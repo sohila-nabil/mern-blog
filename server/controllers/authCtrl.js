3d7015cc6ad979e83633a916c4acf203ea5a4057
sohila-nabil <nsohila03@gmail.com>
2025-02-19 13:37:01 +0200

    updates

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,10 +1,36 @@
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import errorHandler from "../utils/errorHandler.js";
 
-const signup = async(req,res,next)=>{
-    const { username, password, email } = req.body;
-    try {
-        
-    } catch (error) {
-        next(error)
+const signup = async (req, res, next) => {
+  const { username, password, email } = req.body;
+  try {
+    if (
+      !username ||
+      !password ||
+      !email ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      return next(errorHandler(400, "all fields are required"));
     }
-}
+    const user = await User.findOne({ email });
+    if (user) return next(errorHandler(400, "user with this email already exist"));
+    const hashedPassword = await bcrypt.hashSync(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res
+      .status(200)
+      .json({ success: true, message: "user registered successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { signup };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import dbConnection from "./config/dbConnection.js";
-
+import authRouter from "./routes/authRout.js";
 const app = express();
 dbConnection();
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
