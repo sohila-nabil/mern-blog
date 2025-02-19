dcab83329dc287ddf8c8f3df436d4adfd1fd0ad5
sohila-nabil <nsohila03@gmail.com>
2025-02-19 08:29:53 +0200

    create pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
 function App() {
   return (
-    <>
-     Blog
-     
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashbord" element={<Dashboard />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
