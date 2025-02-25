2f518cfa04ff2673a647fad9f27541b12618afb1
sohila-nabil <nsohila03@gmail.com>
2025-02-25 19:46:02 +0200

    updated

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import verifyUser from "../utils/verifyUser.js";
-import { createComment, getPostComments,likeOrDislikeComment } from "../controllers/commentCtrl.js";
+import { createComment, getPostComments,likeOrDislikeComment,deleteComment,editComment } from "../controllers/commentCtrl.js";
 
 const commentRoute = Router();
 
 commentRoute.post("/create-comment", verifyUser, createComment);
 commentRoute.get("/get-comments/:postId", verifyUser, getPostComments);
 commentRoute.put("/like-comment/:commentId", verifyUser, likeOrDislikeComment);
+commentRoute.put("/update/:commentId", verifyUser, editComment);
+commentRoute.delete("/delete/:commentId", verifyUser, deleteComment);
 export default commentRoute;
